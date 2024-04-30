61eece2d54b1c983ad763a6ff2c01bb0b2b37ebc
AbdulmalikKhouj <167778456+AbdulmalikKhouj@users.noreply.github.com>
2024-04-30 19:32:22 +0530

    Delete filter_script.sh

--- filter_script.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --commit-filter '
-if [ "$GIT_AUTHOR_NAME" = "UwaiseAlikhan" ]; then
-    export GIT_AUTHOR_NAME="AbdulmalikKhouj";
-    export GIT_AUTHOR_EMAIL="khoujabdulmalik919@gmail.com";
-    git commit-tree "$@";
-else
-    git commit-tree "$@";
-fi' HEAD
